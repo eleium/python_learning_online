36103be49fd62af9bdb2133af997488c31aa8614
eleium <422875640@qq.com>
2026-03-25 16:31:57 +0800

    优化 .gitignore配置

--- second.py
+++
@@ -1,5 +0,0 @@
-for i in range(10):
-    if i <=4:
-        print('你好啊')
-    else:
-        print('早')
--- ubuntu_lesson/first_ubuntu.py
+++
@@ -1,8 +0,0 @@
-
-print('hello world')
-a=18
-b=20
-c=a+b
-print(c)
-for i in range(10):
-    print(i)
